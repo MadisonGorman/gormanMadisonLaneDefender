8d95129c0690ce74c773cd1f5b93141dc8464394
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2022-08-29 10:09:04 -0500

    Created Repository

---
+++ Assets/Scripts/MainCharacterControls.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterControls : MonoBehaviour
+{
+    public float movementSpeed = 5;
+
+    public KeyCode moveUpFirstOption = KeyCode.UpArrow;
+    public KeyCode moveDownFirstOption = KeyCode.DownArrow;
+
+    public KeyCode moveUpSecondOption = KeyCode.W;
+    public KeyCode moveDownSecondOption = KeyCode.S;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame; Enables the main character to move (up, down)
+    /// </summary>
+    void Update()
+    {
+        float yMovement = Input.GetAxis("Vertical");
+
+        Vector3 newPosition = transform.position;
+
+        // Upon pressing the Up Arrow or W key, the main character moves up at the defined movement speed
+        if (Input.GetKey(moveUpFirstOption) || Input.GetKey(moveUpSecondOption))
+        {
+            newPosition.y += yMovement * Time.deltaTime * movementSpeed;
+
+            transform.position = newPosition;
+        }
+
+        // Upon pressing the Down Arrow or S key, the main character moves down at the defined movement speed
+        if (Input.GetKey(moveDownFirstOption) || Input.GetKey(moveDownSecondOption))
+        {
+            newPosition.y += yMovement * Time.deltaTime * movementSpeed;
+
+            transform.position = newPosition;
+        }
+    }
+}
