c773694a352191821ca8a734fff0fda7e1d0ea5c
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2022-08-31 09:38:53 -0500

    Adjustments related to the Bullet

--- Assets/Scripts/BulletFunctionality.cs
+++ Assets/Scripts/BulletFunctionality.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnBullet : MonoBehaviour
+public class BulletFunctionality : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
