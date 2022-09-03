a8b9a230b50f17f5c16e31696fd0b060c675a8e1
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2022-09-02 19:35:08 -0500

    Implemented Enemy Hit and Death Functionalities

--- Assets/Scripts/EnemyFunctionality.cs
+++ Assets/Scripts/EnemyFunctionality.cs
@@ -8,9 +8,11 @@ public class EnemyFunctionality : MonoBehaviour
 
     public int enemyHealth;
 
-    // public AudioClip enemyHitSound;
+    public Transform enemyTransform;
 
-    // public AudioClip enemyDeathSound;
+    public AudioClip enemyHitSound;
+
+    public AudioClip enemyDeathSound;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,27 @@ public class EnemyFunctionality : MonoBehaviour
         // Referenced: answers.unity.com/questions/690884/how-to-move-an-object-along-x-axis-between-two-poi.html, "How to move an object along x-axis between two points?", Answer provided by robertbu, April 20, 2014 at 11:25PM
         // Results in the enemies continuously moving to the left at a distinct speed upon being spawned
         transform.Translate(Vector2.left * enemyMovementSpeed * Time.deltaTime);
+    }
+
+    // Referenced: "2D Shooting in Unity (Tutorial)" by Brackeys
+    void OnTriggerEnter2D(Collider2D detectCollision)
+    {
+        // Referenced: "2D Shooting in Unity (Tutorial)" by Brackeys
+        Debug.Log(detectCollision.name);
+
+        if (detectCollision.gameObject.name == "Bullet(Clone)")
+        {
+            enemyHealth--;
+
+            AudioSource.PlayClipAtPoint(enemyHitSound, enemyTransform.position);
+
+            if (enemyHealth == 0)
+            {
+                AudioSource.PlayClipAtPoint(enemyDeathSound, enemyTransform.position);
+
+                // Referenced: "2D Shooting in Unity (Tutorial)" by Brackeys
+                Destroy(gameObject);
+            }
+        }
     }
 }
