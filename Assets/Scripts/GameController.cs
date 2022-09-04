13097757a17b827bc548c0330ff9fb9eb4779a1d
MadisonGorman <104847801+MadisonGorman@users.noreply.github.com>
2022-09-03 19:12:51 -0500

    Enabled Random Enemies to Spawn at Random Locations

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+    public Transform[] enemySpawnLocations;
+    
+    // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+    public GameObject[] enemyReferences;
+
+    // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+    int randomEnemySpawnLocations;
+
+    // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+    int randomEnemyReferences;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // InvokeRepeating("SpawnEnemy", 1.0f, 2.0f);
+        InvokeRepeating("SpawnEnemy", 1.0f, 2.00f);
     }
 
     // Update is called once per frame
@@ -20,8 +33,15 @@ public class GameController : MonoBehaviour
         }
     }
 
-    // void SpawnEnemy()
-    //{
+    public void SpawnEnemy()
+    {
+        // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+        randomEnemySpawnLocations = Random.Range(0, enemySpawnLocations.Length);
 
-    //}
+        // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+        randomEnemyReferences = Random.Range(0, enemyReferences.Length);
+
+        // Referenced: "How to Spawn Monsters Randomly from different Spawn Points and Make them Follow the Player in Unity." by Alexander Zotov
+        Instantiate(enemyReferences [randomEnemyReferences], enemySpawnLocations [randomEnemySpawnLocations].position, Quaternion.identity);
+    }
 }
